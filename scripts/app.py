27c7197937c6844acca260943cd1d7d56d9833db
mdaefiol <m.daefiol@gmail.com>
2024-11-14 21:49:43 -0300

    tentativa de criacao de relatorio para os dados, necessario corrigir erro (de formato).

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,18 @@
 import streamlit as st
-from data_processing import load_data, check_files  # para usar o data processing, funcoes especificas
+import sweetviz as sv
+import pandas as pd
+from data_processing import check_files, load_data 
+
+
+# Função para carregar os dados
+@st.cache_data
+def load_data(file_path):
+    try:
+        # Codificação 'ISO-8859-1' (ou 'latin1')
+        data = pd.read_csv(file_path, encoding='ISO-8859-1')
+        return data
+    except Exception as e:
+        return str(e)
 
 # Verificar arquivos disponíveis
 missing_files = check_files()
@@ -11,15 +24,19 @@ else:
     
     # Select para escolher o .csv
     selected_file = st.selectbox("Escolha o arquivo de dados", ["2021.csv", "2022.csv", "2023.csv", "2024.csv"])
-    
-    # Caminho dos arquivos
+
+    # Caminho do arquivo
     file_path = f"/home/hub/Documents/analise_acidentes_de_transito/data/{selected_file}"
     
-    # Carrega e exibe os dados
+    # Carregar os dados
     st.subheader(f"Dados de {selected_file}")
     data = load_data(file_path)
+    
     if isinstance(data, str):  # Se existir erro
         st.error(data)
     else:
-        st.dataframe(data)  # Exibe normalmente
-
+        # Gerar o relatório com Sweetviz
+        report = sv.analyze(data)
+        
+        # Exibir o relatório no Streamlit
+        st.write(report.show_html(), unsafe_allow_html=True)
--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -9,18 +9,19 @@ files = ["2021.csv", "2022.csv", "2023.csv", "2024.csv"]
 def load_data(file_path):
     try:
         # Carregar os dados com 'latin1' e ';'
-        df = pd.read_csv(file_path, encoding='latin1', sep=';', on_bad_lines='skip')
+        df = pd.read_csv(file_path, encoding='latin1', sep=';', on_bad_lines='skip', errors='ignore')
 
         # Verifica se existe dados faltando 
         if df.isnull().values.any():
             df = df.dropna()  # remoção de linhas com dados faltando, como tratar de outra forma? 
-
-        return df.head()  # Retorna as primeiras 20 linhas
+# TODO: remover isso
+        return df.head(20)  # Retorna as linhas
+    
 
     except Exception as e:
         return f"Erro ao carregar os dados de {file_path}: {e}"
 
-# verifica se os arquivos existem
+# Verifica se os arquivos existem
 def check_files():
     missing_files = [file for file in files if not os.path.exists(os.path.join(data_path, file))]
     return missing_files
